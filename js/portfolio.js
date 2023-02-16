5cf357fed38499c63d05b4f2ba4b5c8791cfc8ca
Brad Hammer <30424247+idesovmarch@users.noreply.github.com>
2023-02-16 11:27:54 -0500

    Add files via upload

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,101 +1,25 @@
-$(document).ready(function() {
-  var apiKey = 'UflrFgzS6o0uPRrhT5BLMFCn8o3EP7CR ';
-  var userID = 'idesovmarch';
- 
-
-  (function() {
-    var behanceUserAPI =
-      'https://www.behance.net/v2/users/' + userID + '?callback=?&api_key=' + apiKey;
-
-    function setUserTemplate() {
-      var userData = JSON.parse(sessionStorage.getItem('behanceUser')),
-        getTemplate = $('#profile-template').html(),
-        template = Handlebars.compile(getTemplate),
-        result = template(userData);
-      $('#header').html(result);
-    }
-
-    if (sessionStorage.getItem('behanceUser')) {
-      setUserTemplate();
-    } else {
-      $.getJSON(behanceUserAPI, function(user) {
-        var data = JSON.stringify(user);
-        sessionStorage.setItem('behanceUser', data);
-        setUserTemplate();
-      });
-    }
-  })();
-
-  (function() {
-    var perPage = 12;
-    var behanceProjectAPI =
-      'https://www.behance.net/v2/users/' +
-      userID +
-      '/projects?callback=?&api_key=' +
-      apiKey +
-      '&per_page=' +
-      perPage;
-
-    function setPortfolioTemplate() {
-      var projectData = JSON.parse(sessionStorage.getItem('behanceProject')),
-        getTemplate = $('#portfolio-template').html(),
-        template = Handlebars.compile(getTemplate),
-        result = template(projectData);
-      $('#portfolio').html(result);
-    }
-
-    if (sessionStorage.getItem('behanceProject')) {
-      setPortfolioTemplate();
-    } else {
-      $.getJSON(behanceProjectAPI, function(project) {
-        var data = JSON.stringify(project);
-        sessionStorage.setItem('behanceProject', data);
-        setPortfolioTemplate();
-      });
-    }
-  })();
-
-  $('#portfolio').on('click', '.portfolio-cover', function() {
-    var $this = $(this),
-      projectID = $this.data('project-id'),
-      beProjectContentAPI =
-        'https://www.behance.net/v2/projects/' + projectID + '?callback=?&api_key=' + apiKey,
-      keyName = 'behanceProjectImages-' + projectID;
-
-    function showGallery(dataSource) {
-      $this
-        .magnificPopup({
-          items: dataSource,
-          gallery: {
-            enabled: true
-          },
-          type: 'image',
-          mainClass: 'animated',
-          removalDelay: 350
-        })
-        .magnificPopup('open');
-    }
-
-    if (localStorage.getItem(keyName)) {
-      var srcItems = JSON.parse(localStorage.getItem(keyName));
-      showGallery(srcItems);
-    } else {
-      $.getJSON(beProjectContentAPI, function(projectContent) {
-        var src = [];
-        $.each(projectContent.project.modules, function(index, mod) {
-          if (mod.src != undefined) {
-            src.push({ src: mod.src });
-          }
-        });
-        showGallery(src);
-        var data = JSON.stringify(src);
-        localStorage.setItem(keyName, data);
-      });
-    }
-    $(function() {
-      var $this = $(this),
-      projectID = $this.data('project-id')
-      $('#behance-button').behanceButton(projectID); // replace `1234567` with your actual project's ID
-  })
-  });
+fetch('https://api.mementodatabase.com/v1/libraries/libraryId/entries/?token=CvnVxFvud9AA9kpjjOn2rbpIfy3Jms'
+)
+.then((response) => {
+  if (response.ok) {
+    return response.json();
+  } else {
+    throw new Error("NETWORK RESPONSE NOT OK");
+  }
+})
+.then(function (data) {   
+  console.log(data); 
+  displayPortfolio(data);
+})
+.catch((error) => {
+  console.error("FETCH ERROR:", error);
 });
+
+   function displayPortfolio(data) {
+    const portfolio = data.entries[2];
+    const imageDiv = document.getElementById('prtData');
+   };
+
+   const portImg = document.createElement("img");
+
+   
